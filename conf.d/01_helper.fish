806949eec9152e288fc00aaa8e269d09b048a16c
C. SHEN <chunhua@me.com>
2024-10-27 19:25:48 +1030

    CS: commit save

--- conf.d/01_helper.fish
+++ conf.d/01_helper.fish
@@ -79,13 +79,11 @@ function command_exist
     type -q $argv[1] 2>/dev/null && return 0 || return 1
 end
 
-
 function load_if_exist
     if test -f $argv[1]
         source $argv[1]
     end
 end
-
 
 function is_macOS
     set --local _x (__os)
@@ -107,15 +105,10 @@ function is_MINGW
     return ( string match -q 'MINGW*' "$(uname)" )
 end
 
-
 # Test if an input argument is a symlink
-#
 function is_symlink --argument file
-
     test -L (echo $file  | string replace -r '/$' '')
-
 end
-
 
 function is_string_empty --argument str
     string length --quiet "$str" && return 1 || return 0
@@ -134,6 +127,65 @@ function set_val_or_default --argument val default
         echo $default
     end
 end
+
+
+function clean_config --argument filename -d "Clean up a config file by removing blank lines and comments. For now, comments starts with #"
+
+    set lines (cat $filename)
+    for line in $lines
+        set -l x ( string trim $line  )
+
+        set -l first $( echo $x | string sub -s 1 -e 1 )
+
+        # remove blank and comments
+        if  test "$x" = ""
+            or  test "$first" = '#' 
+            # pass 
+        else 
+            set -a newlines ( echo  $x | awk -F\# '{ print $1}'  )    # remove comments
+        end
+    end
+
+    # fish is not able to return a list/array directory
+    # We need to pack the array into one string, with \n as the delimit
+    printf %s\n $newlines  | string join -- \n
+
+end
+
+# This is a TOML document
+# title = "TOML Example"
+# [owner]
+# name = "Tom Preston-Werner"
+# dob = 1979-05-27T07:32:00-08:00
+# [database]
+# enabled = true
+# ports = [ 8000, 8001, 8002 ]
+# data = [ "delta" ]
+# temp_targets = { cpu = 79.5 }
+function parse_simple_toml --argument input_file --d "Parse simple TOML config into Fish source-able text"
+
+    set newlines ( clean_config $input_file )
+
+    # extract sections in [xxxx]
+    for s in ( printf %s\n $newlines | grep  '^\['  )
+        set -a sections ( echo $s | awk -F\[ '{print $2}' | awk -F\] '{print $1}' )
+    end
+
+    for l in $newlines
+        if echo $l | string match -q "[*]"  
+            set s ( echo $l | awk -F\[ '{print $2}' | awk -F\] '{print $1}' ) 
+            #    echo $s
+        else
+
+            set key ( echo $l | awk -F= '{print $1}' | string trim )
+            set val ( echo $l | cut -d "=" -f2- | string trim | string unescape )
+
+            echo "set  " $s"__"$key  ' '\"$val\"  
+
+        end
+    end
+end
+
 
 #---- Private functions --------------------
 function __os
